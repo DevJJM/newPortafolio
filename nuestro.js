6dfdac1eadac096ee8376f6df72ae0c4cf95066a
DevJJM <jonathanf4.32@hotmail.com>
2022-03-04 16:50:47 -0500

    fisrt version

---
+++ nuestro.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var x = Math.floor(Math.random() * 255);
+    var y = Math.floor(Math.random() * 255);
+    var z = Math.floor(Math.random() * 255);
+
+    document.body.style.backgroundColor = "rgb(" + x + "," + y + "," + z + ")";
+  }
